4026efc0b72784dfcbe36f01f9dfb832427d06a7
BoodyWin <boodywin.official@gmail.com>
2026-04-19 23:10:30 +0200

    Moved to Pathlib, made the variable name more readable, and added a __version__ variable

--- paxer-path-parser.py
+++ paxer-path-parser.py
@@ -1,42 +1,44 @@
 from os import getenv
-from os.path import isdir, normpath, expanduser, realpath
+from pathlib import Path
 from sys import stderr, stdout
+
+__version__ = "0.2-poc"
 
 SYSTEM_DENY_LIST = {
     "/", "/root", "/tmp", "/var/tmp", "/dev", "/sys", "/proc", 
     "/etc", "/var/log", "/dev/shm"
 }
 
-def main(path: str):
-    dirs = path.split(":")
-    new_dirs = []
-    for i in dirs:
-        if i == "." or not i.strip():
+def main(pathRaw: str):
+    pathList = pathRaw.split(":")
+    newPathList = []
+    duplicationList = set()
+    for i in pathList:
+        if not i.strip(): continue
+
+        if i == ".":
             stderr.write(f"Dangerous path `{i}`, skipping\n")
             continue
         
-        norm = normpath(expanduser(i))
-        if norm.startswith("//") and not norm.startswith("///"):
-            norm = "/" + norm.lstrip("/")
-        
-        if norm in new_dirs:
+        normalizedPath = Path(i).expanduser().absolute()
+        normalizedStringPath = str(normalizedPath)
+
+        if normalizedStringPath in duplicationList:
             stderr.write(f"Duplicated path `{i}`, skipping\n")
             continue
         
-        if norm in SYSTEM_DENY_LIST:
-            stderr.write(f"System-reserved path `{i}`, skipping\n")
-            continue
-
-        if not isdir(norm):
+        if not normalizedPath.exists() or not normalizedPath.is_dir():
             stderr.write(f"Non-existing path or not a directory `{i}`, skipping\n")
             continue
 
-        if realpath(norm) in SYSTEM_DENY_LIST:
-            stderr.write(f"Pointed to a system-reserved path `{i}`, skipping\n")
+        if normalizedStringPath in SYSTEM_DENY_LIST or str(normalizedPath.resolve()) in SYSTEM_DENY_LIST:
+            stderr.write(f"System-reserved path or pointed to it `{i}`, skipping\n")
             continue
 
-        new_dirs.append(norm)
-    return ":".join(new_dirs)
+        newPathList.append(i)
+        duplicationList.add(normalizedStringPath)
+
+    return ":".join(newPathList)
 
 
 if __name__ == "__main__":
