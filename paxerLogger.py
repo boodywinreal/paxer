55bc405926e8aaa309729984b82eccbca738b093
BoodyWin <boodywin.official@gmail.com>
2026-05-01 10:16:33 +0300

    Started Working on a custom logger

---
+++ paxerLogger.py
@@ -0,0 +1,17 @@
+from sys import stderr, stdout
+from pathlib import Path
+from const import LOG, MAX_LOG_SIZE
+from os import stat
+
+loggerFile = Path(LOG).expanduser().absolute() / "/paxer.log"
+
+if loggerFile.exists() and stat(loggerFile).st_size > MAX_LOG_SIZE:
+    backup = loggerFile.with_suffix(loggerFile.suffix + ".1")
+    loggerFile.replace(backup)
+
+stream = open(loggerFile, "a")
+
+def verboseCurrent(message: str): pass
+
+def verboseBasic(message: str):
+    pass
