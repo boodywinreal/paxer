50c4da556e2ea2ada439b5e746335c5e3c6a22d9
BoodyWin <boodywin.official@gmail.com>
2026-05-01 10:17:04 +0300

    Now uses the constants, and just have a logLevel variable

--- paxer-path-parser.py
+++ paxer-path-parser.py
@@ -1,45 +1,49 @@
-from os import getenv
-from pathlib import Path
 from sys import stderr, stdout
+from pathlib import Path
+from os import getenv
+from const import *
 
 __version__ = "0.2-poc"
 
-SYSTEM_DENY_LIST = {
-    "/", "/root", "/tmp", "/var/tmp", "/dev", "/sys", "/proc", 
-    "/etc", "/var/log", "/dev/shm"
-}
+logLevel = 1
 
 def main(pathRaw: str):
     pathList = pathRaw.split(":")
     newPathList = []
     duplicationList = set()
+    errorCount = 0
     for i in pathList:
         if not i.strip(): continue
 
         if i == ".":
-            stderr.write(f"Dangerous path `{i}`, skipping\n")
+            stderr.write(DANG.format(i=i) + "\n")
+            errorCount += 1
             continue
         
         normalizedPath = Path(i).expanduser().absolute()
         normalizedStringPath = str(normalizedPath)
 
         if normalizedStringPath in duplicationList:
-            stderr.write(f"Duplicated path `{i}`, skipping\n")
+            stderr.write(DUP.format(i=i) + "\n")
+            errorCount += 1
             continue
         
         if not normalizedPath.exists() or not normalizedPath.is_dir():
-            stderr.write(f"Non-existing path or not a directory `{i}`, skipping\n")
+            stderr.write(NON.format(i=i) + "\n")
+            errorCount += 1
             continue
 
         if normalizedStringPath in SYSTEM_DENY_LIST or str(normalizedPath.resolve()) in SYSTEM_DENY_LIST:
-            stderr.write(f"System-reserved path or pointed to it `{i}`, skipping\n")
+            stderr.write(SYS.format(i=i) + "\n")
+            errorCount += 1
             continue
 
         newPathList.append(i)
         duplicationList.add(normalizedStringPath)
-
+    if errorCount != 0:
+        stderr.write(EXT.format(count=errorCount) + "\n")
+    
     return ":".join(newPathList)
-
 
 if __name__ == "__main__":
     if path := getenv("PATH"):
