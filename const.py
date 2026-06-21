20004e4bca4bf37b93fae6e3afb0fcfa86cf151c
BoodyWin <boodywin.official@gmail.com>
2026-06-21 15:15:03 +0300

    Code improvements, added a custom PrintHelp, and other minor tweaks

--- const.py
+++ const.py
@@ -1,19 +1,21 @@
+from pathlib import Path
+
 VERSION = "1.0-py"
 
 LOG = "~/.paxer/logs"
 MAX_LOG_SIZE = 5 * 1024 * 1024
 
-DANG = "Dangerous path `{i}`, skipping"
-DUP = "Duplicated path or symlink `{i}`, skipping"
-NON = "Non-existing path or not a directory `{i}`, skipping"
-SYS = "System-reserved path `{i}`, skipping"
-SYM_DANG = "Symlink `{i}` poins to dangerous path `{real}`, skipping"
-SYM_DUP = "Symlink `{i}` points to an already existing path `{real}`, skipping"
-SYM_NON = "Symlink `{i}` points to non-existing directory `{real}`, skipping"
-SYM_SYS = "Symlink `{i}` points to a system-reserved path `{real}`, skipping"
-EXT = "Finished with {count} issues, see " + LOG + "/log.txt" + " for more output"
+DANG = "Dangerous path `{}`, skipping"
+DUP = "Duplicated path or symlink `{}`, skipping"
+NON = "Non-existing path or not a directory `{}`, skipping"
+SYS = "System-reserved path `{}`, skipping"
+SYM_DANG = "Symlink `{}` poins to dangerous path `{}`, skipping"
+SYM_DUP = "Symlink `{}` points to an already existing path `{}`, skipping"
+SYM_NON = "Symlink `{}` points to non-existing directory `{}`, skipping"
+SYM_SYS = "Symlink `{}` points to a system-reserved path `{}`, skipping"
+EXT = "Finished with {} issues, see " + LOG + "/log.txt" + " for more output"
 
-SYSTEM_DENY_LIST = {
-    "/", "/root", "/tmp", "/var/tmp", "/dev", "/sys", "/proc", 
+SYSTEM_DENY_LIST = {Path(i) for i in {
+    "/root", "/tmp", "/var/tmp", "/dev", "/sys", "/proc", 
     "/etc", "/var/log", "/dev/shm"
-}
+}}
--- paxer-path-parser.py
+++ paxer-path-parser.py
@@ -7,6 +7,11 @@ import argparse
 
 __version__ = VERSION
 
+def PrintHelp(parser, file):
+    parser._print_message(parser.format_help(), file)
+    exit(1)
+
+
 def HandleArguments():
     parser = argparse.ArgumentParser(
         prog="PAXER (PAth fiXER)"
@@ -14,7 +19,7 @@ def HandleArguments():
     parser.add_argument("--path", "-p", default=getenv("PATH"), type=str, help="Give a custom path to handle")
     parser.add_argument("--verbose", "-v", action="store_true", help="Show all hidden warnings and errors")
     parser.add_argument("--silent", "-s", action="store_true", help="Runs the tool silently")
-    parser.print_help = lambda file=stderr: parser._print_message(parser.format_help(), file)
+    parser.print_help = lambda file=stderr: PrintHelp(parser, file)
 
     return parser.parse_args()
 
@@ -28,74 +33,73 @@ def main():
     if data.path is None:
         pxl.normalError("Path is None")
         print(getenv("PATH"), end="")
-        return
+        exit(1)
     
-    pathList = data.path.split(":")
-    newPathList = []
-    dupCheckSet = set()
+    paths = [Path(i.strip()) for i in data.path.split(":") if i.strip()]
+    newest = []
+    duplication = set()
 
-    errorCount = 0
+    errors = 0
 
-    for path in pathList:
-        if not path.strip(): continue
-        if path == ".":
-            pxl.pathUnusableError(DANG.format(i=path))
-            errorCount += 1
+    for raw in paths:
+        if raw == Path("."):
+            pxl.pathUnusableError(DANG.format(raw))
+            errors += 1
+            continue
+        if raw == Path("/"):
+            pxl.pathUnusableError(SYS.format(raw))
+            errors += 1
             continue
 
-        normPath = Path(path).expanduser().resolve()
-        normStringPath = str(normPath)
-
-        if normStringPath in dupCheckSet:
-            pxl.pathUnusableError(DUP.format(i=path))
-            errorCount += 1
+        normal = raw.expanduser().absolute()
+        if normal in duplication:
+            pxl.pathUnusableError(DUP.format(raw))
+            errors += 1
             continue
-        dupCheckSet.add(normStringPath)
+        duplication.add(normal)
 
-        if normPath.is_symlink():
-            realPath = normPath.readlink()
-            realStringPath = str(realPath)
+        if normal.is_symlink():
+            real = (normal.parent / normal.readlink()).resolve()
+            if real in duplication:
+                pxl.pathUnusableError(SYM_DUP.format(raw, real))
+                errors += 1
+                continue
+            duplication.add(real)
 
-            if realStringPath == ".":
-                pxl.pathUnusableError(DANG.format(i=path, real=realStringPath))
-                errorCount += 1
+            if not real.is_dir():
+                pxl.pathUnusableError(SYM_NON.format(raw, real))
+                errors += 1
                 continue
 
-            realPath = realPath.expanduser().resolve()
-            realStringPath = str(realPath)
-
-            if realPath in dupCheckSet:
-                pxl.pathUnusableError(SYM_DUP.format(i=path, real=realStringPath))
-                errorCount += 1
-                continue
-            dupCheckSet.add(realPath)
+            forbidden = (
+                real in SYSTEM_DENY_LIST
+                or any(parent in SYSTEM_DENY_LIST for parent in real.parents)
+            )
             
-            if not realPath.is_dir():
-                pxl.pathUnusableError(SYM_NON.format(i=path, real=realStringPath))
-                errorCount += 1
+            if forbidden:
+                pxl.pathUnusableError(SYM_SYS.format(raw))
+                errors += 1
                 continue
-
-            if realStringPath in SYSTEM_DENY_LIST:
-                pxl.pathUnusableError(SYM_SYS.format(i=path, real=realStringPath))
-                errorCount += 1
-                continue
-
         else:
-            if not normPath.is_dir():
-                pxl.pathUnusableError(NON.format(i=path))
-                errorCount += 1
+            if not normal.is_dir():
+                pxl.pathUnusableError(NON.format(raw))
+                errors += 1
                 continue
+            
+            forbidden = (
+                normal in SYSTEM_DENY_LIST
+                or any(parent in SYSTEM_DENY_LIST for parent in normal.parents)
+            )
 
-            if normStringPath in SYSTEM_DENY_LIST:
-                pxl.pathUnusableError(SYS.format(i=path))
-                errorCount += 1
+            if forbidden:
+                pxl.pathUnusableError(SYS.format(raw))
+                errors += 1
                 continue
-        
-        newPathList.append(path)
-    if errorCount != 0:
-        pxl.normalError(EXT.format(count=errorCount) + "\n")
+        newest.append(raw)
     
-    print(":".join(newPathList), end="")
+    if errors != 0: pxl.normalError(EXT.format(count=errors) + "\n")
+    print(":".join(newest), end="")
+    exit(0)
 
 if __name__ == "__main__":
     main()
